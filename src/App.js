e74dc9d54f035d39d1dd1b5932bbedc940992006
cryptofifa2023 <crypto.fifa23@gmail.com>
2022-06-16 16:12:16 +1000

    version1.2

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from "./coin.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="blog-graphic-text">Welcome to CFIFA 23 !</div>
+      <div style={{ marginTop: 50, marginBottom: 50 }}>
+        <a
+          className="marketing-button"
+          href="https://presale.cfifa.finance"
+          target="_self"
+          rel="noopener noreferrer"
+        >
+          Launch Presale
+        </a>
+      </div>
+      <div>
+        <a
+          className="marketing-button"
+          href="https://gitbook.com/cryptofifa2023"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Whitepaper
+        </a>
+      </div>
+      <div style={{ marginTop: 50, marginBottom: 50 }}>
+        <a
+          className="marketing-button"
+          href="https://t.me/cfifa_official"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Telegram
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
